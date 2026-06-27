fe629bca7e109334b4f83c81af46938bab71ed54
khadharzolwyerurolg-eng <khadharzolwyerurolg@gmail.com>
2026-06-27 08:19:29 +0200

    Aula-03: Modificadores de Acesso

---
+++ Aula_03/Bhaskara.php
@@ -0,0 +1,64 @@
+<?php
+
+class Bhaskara
+{
+    /**
+     * @var float $a, $b, $c, $delta;
+     */
+    private $a, $b, $c, $delta;
+    
+    /**
+     * Calcula a forma de Bhashara:
+     * 
+     * @param float $a, $b, $c;
+     * 
+     * @return array|float[]
+     */
+    
+    public function calcular(float $a, float $b, float $c)
+    {
+        $this->a = $a;
+        $this->b = $b;
+        $this->c = $c;
+
+        $this->calcularDelta();
+        $this->validarDelta();
+        $x1 = $this->calcularX1();
+        $x2 = $this->calcularX2();
+
+        if($x1 === $x2)
+            return [$x1];
+        else
+            return [$x1, $x2];
+    }
+
+    private function calcularDelta(): void 
+    {
+        $this->delta = ($this->b * $this->b) - (4 * $this->a * $this->c);
+    }
+
+    private function validarDelta(): void
+    {
+        if($this->delta < 0){
+            throw new Exception("ERRO: Valores de A, B e C resultam num Delta negativo.\n");
+        }
+    }
+
+    private function calcularX1(): float
+    {
+        return (
+            (($this->b * -1) + sqrt($this->delta))
+            /
+            (2 * $this->a)
+        );
+    }
+
+    private function calcularX2(): float
+    {
+        return (
+            (($this->b * -1) - sqrt($this->delta))
+            /
+            (2 * $this->a)
+        );
+    }
+}
---
+++ Aula_03/README.md
@@ -0,0 +1,17 @@
+## O que são Modificadores de Acesso?
+
+São palavras-chave que controlam quem pode ver ou alterar as informações dentro de uma classe. Eles servem para garantir o encapsulamento, que é a proteção dos dados do objeto.
+
+## Os Três Modificadores Principais
+
+    ## Público (public):
+        Visibilidade: Total.
+        Acesso: Qualquer outra classe do sistema pode aceder e modificar. 
+
+    ## Privado (private)
+        Visibilidade: Restrita.
+        Acesso: Apenas a própria classe onde foi criado pode aceder. Classes externas não têm acesso direto. 
+        
+    Protegido (protected)
+        Visibilidade: Intermédia.
+        Acesso: Apenas a própria classe e as suas subclasses (herança) podem aceder.
